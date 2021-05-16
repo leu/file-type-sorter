447feda2daea676dc84cbfa7a13ff223cde2d19a
Daniel <daniel.simols@outlook.com>
2021-05-16 12:03:01 +0200

    luna suggestions

--- src/io/github/leu/FileSort.java
+++ src/io/github/leu/FileSort.java
@@ -3,31 +3,29 @@ package io.github.leu;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
 
 public class FileSort {
   FileSystem fileSystem = FileSystems.getDefault();
   File rootFileDir;
   HashMap<String, int[]> fileTypeFolderSize = new HashMap<>();
-  public static String[] UNWANTED_FILETYPES = {"7z", "ai", "asp", "bat", "c", "csv", "db", "doc",
+  public static Set<String> UNWANTED_FILETYPES = new HashSet<>(Arrays.asList("7z", "ai", "asp", "bat", "c", "csv", "db", "doc",
           "f", "go", "h", "ini", "jar", "java", "json", "jsp", "odg", "php", "pl",
-          "ppt", "sxw", "tex", "vsd", "xls", "zip"};
+          "ppt", "sxw", "tex", "vsd", "xls", "zip"));
   public static int MAX_FOLDER_SIZE = 200;
 
-  FileSort(String rootDir) {
+  public FileSort(String rootDir) {
     rootFileDir = fileSystem.getPath(rootDir).toFile();
   }
 
-  public void run() {
-    recursiveRun(rootFileDir, Objects.requireNonNull(rootFileDir.listFiles()));
+  public void sortRootDirectory() {
+    recursiveSort(rootFileDir, Objects.requireNonNull(rootFileDir.listFiles()));
   }
 
-  private void recursiveRun(File root, File[] files) {
+  private void recursiveSort(File root, File[] files) {
     for (File subFile : Objects.requireNonNull(files)) {
       if (subFile.isDirectory()) {
-        recursiveRun(root, subFile.listFiles());
+        recursiveSort(root, subFile.listFiles());
         if (Objects.requireNonNull(subFile.listFiles()).length == 0) {
           try {
             Files.delete(subFile.toPath());
@@ -36,12 +34,12 @@ public class FileSort {
           }
         }
       } else if (subFile.isFile()) {
-        fileSort(root, subFile);
+        sortFile(root, subFile);
       }
     }
   }
 
-  private void fileSort(File root, File file) { //PRE: file has format name.(extension)
+  private void sortFile(File root, File file) { //PRE: file has format name.(extension)
     String fileType = file.getName().split("\\.")[1];
     if (!fileTypeFolderSize.containsKey(fileType)) {
       int[] initialFolderSize = {0, 0}; //{folderNum, numOfFilesInFolder}
@@ -55,7 +53,7 @@ public class FileSort {
     Path destinationFile = fileSystem.getPath(folderPath + "/" + file.getName());
 
     try {
-      if (Arrays.asList(UNWANTED_FILETYPES).contains(fileType)) {
+      if (UNWANTED_FILETYPES.contains(fileType)) {
         Files.delete(file.toPath());
       } else {
         if (!Files.exists(destinationFolder)) {
--- src/io/github/leu/Main.java
+++ src/io/github/leu/Main.java
@@ -3,6 +3,6 @@ package io.github.leu;
 public class Main {
   public static void main(String[] args) {
     FileSort fileSort = new FileSort("/media/leu/574b57ee-84cb-46a3-baa1-b585b2ac3247/windows-home");
-    fileSort.run();
+    fileSort.sortRootDirectory();
   }
 }
